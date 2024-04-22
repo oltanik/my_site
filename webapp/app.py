fb8aaf3373b09ada803b7a7ae46ab9dbac225fc5
adalanthe <adalathe@gmail.com>
2024-04-22 09:56:57 +0300

    Релиз авторизации

--- webapp/app.py
+++ webapp/app.py
@@ -1,20 +1,38 @@
-from flask import Flask, render_template
-from webapp.db import db, init_app_db
-from .models.user import User
-from .authorization import authorization
-
-app = Flask(__name__)
-app.config.from_pyfile('config.py')
-init_app_db(app, db)
-
-@app.route('/')
-def index():
-    page_title = app.config['TEXT_TITLE']
-    text = app.config['TEXT']
-    return render_template('main_page.html', page_title=page_title, text=text, users=User.query.all())
+from flask import Flask, request, current_app
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_babel import Babel, lazy_gettext as _l
 
 
-app.register_blueprint(authorization)
+def get_locale():
+    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
+
+db = SQLAlchemy()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = _l('Please log in to access this page.')
+babel = Babel()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+
+    db.init_app(app)
+    with app.app_context():
+        # db.drop_all()
+        db.create_all()
+    login.init_app(app)
+    babel.init_app(app, locale_selector=get_locale)
+
+    from webapp.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from webapp.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    return app
+
+from webapp import models
 
 """
 Запуск сервера:
---
+++ webapp/models.py
@@ -0,0 +1,69 @@
+from datetime import datetime, timezone, timedelta
+from webapp.app import db, login
+import secrets
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlalchemy as sa
+from flask_login import UserMixin
+
+
+# Таблица с пользователями
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    token = db.Column(db.String(32), unique=True, nullable=True)
+    token_expiration = db.Column(db.DateTime())
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def to_dict(self, include_email=False):
+        data = {
+            'id': self.id,
+            'username': self.username
+        }
+        if include_email:
+            data['email'] = self.email
+        return data
+
+    def from_dict(self, data, new_user=False):
+        for field in ['username', 'email']:
+            if field in data:
+                setattr(self, field, data[field])
+        if new_user and 'password' in data:
+            self.set_password(data['password'])
+
+    def get_token(self, expires_in=3600):
+        now = datetime.now(timezone.utc)
+        if self.token and self.token_expiration.replace(
+                tzinfo=timezone.utc) > now + timedelta(seconds=60):
+            return self.token
+        self.token = secrets.token_hex(16)
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.now(timezone.utc) - timedelta(
+            seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = db.session.scalar(sa.select(User).where(User.token == token))
+        if user is None or user.token_expiration.replace(
+                tzinfo=timezone.utc) < datetime.now(timezone.utc):
+            return None
+        return user
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
